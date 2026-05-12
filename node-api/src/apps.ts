f15bbef34d7a8b6ed59424bb9840d3ebb5ab4076
ronju <gilbertzu8@gmail.com>
2026-05-12 21:49:48 +0700

    Set Up Prisma for DB

--- node-api/src/apps.ts
+++ node-api/src/apps.ts
@@ -8,6 +8,8 @@ app.use(express.json());
 
 app.get("/", (_, res) =>{
     res.json({message: "Signalboard API Running"})
-})
-
+});
+// basically _ telling the dev that the Request doesnt need to be used
+// can be changed to a variable to be use
+// res is a variable to be set to send response as callback
 export default app
